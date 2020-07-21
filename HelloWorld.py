274aedb55c06dd91223d467ce2424b58c689cfcc
esteban_win <estebanalzated@gmail.com>
2020-07-21 21:22:36 +1000

    otra vez y otra vez

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,5 @@
-###############THIS IS THE NEW FILE OH YEAH
+###############THIS IS THE NEW FILE OH YEAH FROM UBUNTU
+# ah si pues yo tambien lo modifico desde Windows
 #Hello World
 print("Mi segundo Hello World")
 #This is from git bash
